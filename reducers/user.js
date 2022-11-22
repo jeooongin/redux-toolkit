c83620396f222e0e49c94cfea47b1eaf768acaaf
jeongin <kwjddls1919@naver.com>
2022-11-22 13:17:28 +0900

    Feat: 더미데이터, 로그인 비동기 액션 생성

--- reducers/user.js
+++ reducers/user.js
@@ -1,22 +1,53 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+
+// 더미 데이터
+const dummyUser = (data) => ({
+  email: data.email,
+  password: data.password,
+});
 
 // initialState
 const initialState = {
-  logInSuccess: false,
+  me: null,
+  logInLoading: false,
+  logInDone: false,
+  logInError: null,
 };
+
+// 비동기 액션
+export const logIn = createAsyncThunk("user/login", async (data, thunkAPI) => {
+  try {
+    const response = await dummyUser(data);
+    return response;
+  } catch (error) {
+    return thunkAPI.rejectWithValue(error.response.data);
+    e;
+  }
+});
 
 // reducer
 const userSlice = createSlice({
   name: "user",
   initialState,
-  reducers: {
-    logIn(state) {
-      state.logInSuccess = true;
-    },
-    logOut(state) {
-      state.logInSuccess = false;
-    },
-  },
+  reducers: {},
+  extraReducers: (builder) =>
+    builder
+      .addCase(logIn.pending, (state) => {
+        state.logInLoading = true;
+        state.logInDone = false;
+        state.logInError = null;
+      })
+      .addCase(logIn.fulfilled, (state, action) => {
+        state.logInLoading = false;
+        state.logInDone = true;
+        state.logInError = null;
+        state.me = action.payload;
+      })
+      .addCase(logIn.rejected, (state, action) => {
+        state.logInLoading = false;
+        state.logInDone = false;
+        state.logInError = action.payload;
+      }),
 });
 
 export default userSlice;
