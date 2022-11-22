92a29095749cc0e04135c971609fccc9f0c12b02
jeongin <kwjddls1919@naver.com>
2022-11-22 13:16:56 +0900

    Design: index 페이지 UI 변경

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,12 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
-import LogIn from "../components/LogIn";
+import LogInForm from "../components/LogInForm";
 
 const index = () => {
-  return (
-    <>
-      <LogIn />
-    </>
-  );
+  const { me } = useSelector((state) => state.user);
+
+  return <>{me ? <p>로그인 성공</p> : <LogInForm />}</>;
 };
 
 export default index;
